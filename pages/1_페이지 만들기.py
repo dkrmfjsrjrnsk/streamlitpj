ea4eae9c80782f21b1dbd617bb2d786bf8a8b695
choigeon <98gun@naver.com>
2023-01-30 11:07:32 +0900

    commit

---
+++ pages/1_페이지 만들기.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(
+    page_title='프로토타입_1',
+    page_icon='🎈',
+    layout='wide'
+)
+
+st.title('📌퇴직연금 OCIO 포트폴리오 전략')
---
+++ pages/2_페이지 만들기.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+import streamlit as st
+st.set_page_config(
+    page_title='프로토타입_2',
+    page_icon='🎈',
+    layout='wide'
+)
+
+st.title('📌퇴직연금 OCIO 포트폴리오 전략')
+st.title('📌퇴직연금 OCIO 포트폴리오 전략')
